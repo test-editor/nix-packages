ad1c6ad3a98b5f00fcfe8edde2a8d80f0428a1fc
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-11-19 17:49:27 +0100

    add firefox 62.0.3

---
+++ pkgs/firefox_62_0_3/default.nix
@@ -0,0 +1,18 @@
+{ stdenv, bash, findutils, fetchurl }:
+
+firefox_62_0_3 = stdenv.mkDerivation rec {
+    name = "firefox_62_0_3";
+    version = "62.0.3";
+    src = fetchurl {
+      url = "http://ftp.mozilla.org/pub/firefox/releases/62.0.3/linux-x86_64/en-US/firefox-62.0.3.tar.bz2";
+      sha256 = "dafff4bd8b45d82f861c2e7215963461ed8333d75534defe677c3deefb2b3aa2";
+    };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ./* "$out/bin/"
+    # correct interpreter and rpath for binaries to work
+    find $out -type f -perm -0100 \
+        -exec patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \;
+   '';
+};
