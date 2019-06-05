ca17c00a2716ed01f280dc824762ebcd08950a63
kbayraktar <kbayraktar@users.noreply.github.com>
2019-06-05 13:37:28 +0200

    Firefox 67.0.0 added as latest Nix Browser

--- default.nix
+++ default.nix
@@ -5,8 +5,9 @@
   # pkgs
   openjdk_10_0_2 = pkgs.callPackage ./pkgs/openjdk_10_0_2 {};
   firefox_62_0_3 = pkgs.callPackage ./pkgs/firefox_62_0_3 {};
+  firefox_67_0_0 = pkgs.callPackage ./pkgs/firefox_67_0_0 {};
   firefox_60_2_2_esr = pkgs.callPackage ./pkgs/firefox_60_2_2_esr {};
 
-  firefox_latest = pkgs.callPackage ./pkgs/firefox_62_0_3 {};
+  firefox_latest = pkgs.callPackage ./pkgs/firefox_67_0_0 {};
   firefox_esr = pkgs.callPackage ./pkgs/firefox_60_2_2_esr {};
 }
